44572e19b50879601a7fea32780258b2bb5f51c2
Sam Galison <samface@gmail.com>
2017-11-07 10:47:41 -0500

    in class

--- teaching/cc2017/week11/main.js
+++ teaching/cc2017/week11/main.js
@@ -1,12 +1,56 @@
-var namestring = "xiujie bi,jess,vincent,analisse,inga,tiffany,qiyao,jiayi,dan,meredith,khe,yasmin,lily,yuchen,jeanette"
-console.log(namestring)
+var hipsterstring = "Mixtape tbh taxidermy schlitz DIY meh, tumblr trust fund shoreditch gochujang paleo palo santo. Kickstarter humblebrag kale chips celiac, vaporware gastropub migas post-ironic gluten-free snackwave synth slow-carb."
+console.log(hipsterstring)
+
+var hipArray = hipsterstring.split(" ")
+console.log(hipArray)
+
+// var newstring = hipArray.join("PUPPY")
+// console.log(newstring)
 
 
-var nameArray = namestring.split(",")
-console.log(nameArray)
+var tupperware = document.querySelector(".output")
+
+console.log(tupperware)
+
+// var word1 = document.createElement("p")
+// word1.textContent = hipArray[0]
+// tupperware.appendChild(word1)
+
+// var word2 = document.createElement("p")
+// word2.textContent = hipArray[1]
+// tupperware.appendChild(word2)
+
+// var word3 = document.createElement("p")
+// word3.textContent = hipArray[2]
+// tupperware.appendChild(word3)
 
 
-var container = document.querySelector(".output ul")
+for (var i = 0; i < hipArray.length; i++) {
+	var box = document.createElement("div")
+	var image = document.createElement("img")
+	var caption = document.createElement("p")
+
+	var tinybox = document.createElement("div")
+	var link = document.createElement("a")
+
+	image.src="images/grumpy-cat.jpg"
+
+	caption.textContent = hipArray[i]
+
+	tinybox.appendChild(link)
+
+
+	box.appendChild(tinybox)
+	box.appendChild(image)
+	box.appendChild(caption)
+
+
+	tupperware.appendChild(box)
+}
+
+
+
+
 
 // for(var i = 0; i < nameArray.length; i++) {
 // 	console.log(nameArray[i])
